1ad2afad2ed1737f21b6655d925f7fdf47f15c6a
Héctor José Calderón Pachón <hj.calderon10@uniandes.edu.co>
2016-08-30 19:30:04 -0500

    Controlando ando

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,11 @@
 
 # --- !Ups
 
+create table campoentity (
+  id                        bigint not null,
+  constraint pk_campoentity primary key (id))
+;
+
 create table pozo (
   id                        bigint not null,
   estado                    integer,
@@ -21,9 +26,21 @@ create table sensor (
   constraint pk_sensor primary key (id))
 ;
 
+create table usuarioentity (
+  id                        bigint not null,
+  nombre                    varchar(255),
+  tipo                      integer,
+  constraint ck_usuarioentity_tipo check (tipo in (0,1)),
+  constraint pk_usuarioentity primary key (id))
+;
+
+create sequence Campo;
+
 create sequence Pozo;
 
 create sequence Sensor;
+
+create sequence Usuario;
 
 alter table sensor add constraint fk_sensor_pozo_1 foreign key (pozo_id) references pozo (id) on delete restrict on update restrict;
 create index ix_sensor_pozo_1 on sensor (pozo_id);
@@ -34,13 +51,21 @@ create index ix_sensor_pozo_1 on sensor (pozo_id);
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists campoentity;
+
 drop table if exists pozo;
 
 drop table if exists sensor;
 
+drop table if exists usuarioentity;
+
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists Campo;
 
 drop sequence if exists Pozo;
 
 drop sequence if exists Sensor;
+
+drop sequence if exists Usuario;
 
